fb5aff647f3fc90b52290800d39ade70c9fec8be
bradofferman <164579005+bradofferman@users.noreply.github.com>
2025-06-26 16:35:03 -0700

    deploy_0626_1634

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/FP-PlantShopping-AppReact",
+  base: "/paradisenursery",
   plugins: [react()],
 })
